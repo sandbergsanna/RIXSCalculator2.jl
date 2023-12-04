b193a133944eb0288f37bd060ca735a4d78a42df
sandbergsanna <sandbergsanna@gmail.com>
2023-12-04 11:17:23 +0100

    add multi site operators

--- src/operators/operators_sp/operators_sp.jl
+++ src/operators/operators_sp/operators_sp.jl
@@ -3,3 +3,7 @@ include("operators_sp_abstract_type.jl")
 
 # single site
 include("single_site/operators_sp_single_site_abstract_type.jl")
+
+# multi_site
+include("multi_site/operators_sp_multi_site_abstract_type.jl")
+include("multi_site/sp_local_multisite_operator.jl")
