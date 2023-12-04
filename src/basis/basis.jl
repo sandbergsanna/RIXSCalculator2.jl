b11d3fd5e9d991709defb6dd1ee707aa3fdb2b8d
sandbergsanna <sandbergsanna@gmail.com>
2023-12-04 13:57:46 +0100

    add multi particle basis

--- src/basis/basis.jl
+++ src/basis/basis.jl
@@ -11,3 +11,6 @@ include("basis_abstract_type.jl")
 
 # single particle basis definitions
 include("basis_sp/basis_sp.jl")
+
+# multi particle basis definitions
+include("basis_mp/basis_mp.jl")
---
+++ src/basis/basis_mp/basis_mp.jl
@@ -0,0 +1,11 @@
+# include explicit MP basis states
+include("basisstate_mp_type_definition.jl")
+
+# include concrete MP basis type
+include("basis_mp_type_definition.jl")
+
+
+# include multi particle functions
+include("mp_functions/permutation_functions.jl")
+include("mp_functions/lookup_usage_functions.jl")
+include("mp_functions/functions.jl")
